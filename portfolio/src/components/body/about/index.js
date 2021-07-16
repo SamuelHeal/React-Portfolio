1b16dd3789b44f4149cdbddafe2d269f46e246a2
Samuel Heal <samuelheal@hotmail.com>
2021-07-16 17:15:01 +1000

    about section complete

--- portfolio/src/components/body/about/index.js
+++ portfolio/src/components/body/about/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import './about.css'
+import samPic from './sam-pic.jpeg'
 
 function About() {
     return (
-        <div>
-            About
+        <div className='aboutContainer'>
+            <div className='aboutContentContainer'>
+                
+                <div className="aboutPara">
+                    <div className="aboutHeading">
+                        About
+                    </div>
+                    I am a coding fanatic who has recently begun a journey in Web Development 
+                    with the dream of becoming a fully fledged Software Engineer. I am incredibly 
+                    passionate about what I do and am nearing the completion of a six month bootcamp 
+                    in Web Development that has been hosted by Trilogy Education through the University 
+                    of Sydney. Here, I have learnt the necessary skills required to become a Full-Stack 
+                    Web Developer. To further add to my education, I have been accepted into a Bachelor 
+                    of Design Computing/Advanced Studies, the Advanced Studies component will be used to 
+                    complete a Major in Software Development. I am beyond excited to see where my love 
+                    for this industry takes me.
+                </div>
+                <div className='picContainer'>
+                    <img className="aboutPic" src={samPic}></img>
+                </div>
+                
+                
+            </div>
+           
         </div>
     )
 }
