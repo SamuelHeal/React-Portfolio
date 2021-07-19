7b254a570a726c372f1941806dcf6d5bc2622484
Samuel Heal <samuelheal@hotmail.com>
2021-07-19 11:04:53 +1000

    portfolio gallery item styled

--- portfolio/src/components/body/portfolio/index.js
+++ portfolio/src/components/body/portfolio/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './portfolio.css'
+import gymClub from './gymClub.png'
 
 function Portfolio() {
     return (
         <div>
             Portfolio
+
+            <div className="projContainer">
+                <div className="projInfoContainer">
+                    <h2>Gym Club</h2>
+                    <p className="projCat">Full-Stack</p>
+                    <p className="projSpec"><span>Languages</span><br /><br />HTML - CSS - Javascript - mySQL<br /></p>
+                    <div className='linkContainer'>
+                        <a href='https://github.com/SamuelHeal/gym-club' target='_blank' rel="noreferrer" className="projGit">Github</a>
+                        <a href='https://gym-club-group-project.herokuapp.com/' target='_blank' rel="noreferrer" className="projGit">Website</a>
+                    </div>
+                </div>
+                <img className='img' src={gymClub} alt='Gym Club'/>
+            </div>
         </div>
     )
 }
