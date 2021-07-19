d2ab0273c1a319ed0796b4a21f6a408bebdb7cc9
Samuel Heal <samuelheal@hotmail.com>
2021-07-19 13:20:54 +1000

    custom cursor added

--- portfolio/src/components/home/index.js
+++ portfolio/src/components/home/index.js
@@ -4,10 +4,21 @@ import Header from '../header/index'
 import Body from '../body/index'
 import Footer from '../footer/index'
 import { SocialIcon } from 'react-social-icons';
+import AnimatedCursor from 'react-animated-cursor';
 
 function Home() {
     return (
         <div className='homeContainer'>
+            <div className="cursor">
+                <AnimatedCursor
+                innerSize={16}
+                outerSize={12}
+                color='195, 7, 63'
+                outerAlpha={0.8}
+                innerScale={1}
+                outerScale={5}
+                />
+            </div>
             <div className='sticky1'>
                 <div className='cover'></div>
                 <Header />
